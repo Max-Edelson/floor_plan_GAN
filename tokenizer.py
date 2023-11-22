ca4e78702ba50281ce23a2a1d75b6b47b7315d36
Max-Edelson <meedelson@gmail.com>
2023-11-22 15:05:19 -0800

    Made tokenizer object

--- tokenizer.py
+++ tokenizer.py
@@ -2,45 +2,64 @@ import re
 import os
 from torch.utils.data import Dataset
 import torch
+import json 
 
-token_to_id = {} # TODO This has to be saved with each model, as mappings may vary from model to model
-ctr = 0
+class Tokenizer(object):
+    def __init__(self, tokenizer_meta_data=os.join('data', 'tokenizer_meta_data.json')):
+        self.token_to_id = {} # TODO This has to be saved with each model, as mappings may vary from model to model
+        self.ctr = 0
+        if os.path.isfile(tokenizer_meta_data): # tokenizer_meta_data file already exists. Load it in
+            meta_data = json.load(open(tokenizer_meta_data,))
+            self.token_to_id = meta_data['token_to_id']
+            self.ctr = meta_data['ctr']
 
-start_token = re.escape('<?xml version="1.0"?>\n<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"')
-pattern = f'(style="[^"]*")|([\w:-]+=)|(rgb|none|rotate|layer|matrix|default|visible|px|em|block|crosshair|translate)|(<)(\w+)|(/>)|(</\w+>)|({start_token})|(.)'
-# Requires that we remove text, desc, class, id, and extra spaces in pre-processing
-def custom_tokenizer(text):
-    matches = re.findall(pattern, text, re.DOTALL)
-    res = []
-    for group in matches:
-        if group[0]:
-            styles = re.findall(r'(style=)(")|(rgb|none|rotate|layer|matrix|default|visible|px|em|block|crosshair|translate)|([a-zA-Z0-9-]*:)|(.)', group[0], re.DOTALL)
-            for match in styles:
-                for i in match:
-                    if i not in [None, '']:
-                        res.append(i)
-            continue
-        for match in group:
-            if match not in [None, '']:
-                res.append(match)
+        self.start_token = re.escape('<?xml version="1.0"?>\n<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"')
+        self.pattern = f'(style="[^"]*")|([\w:-]+=)|(rgb|none|rotate|layer|matrix|default|visible|px|em|block|crosshair|translate)|(<)(\w+)|(/>)|(</\w+>)|({self.start_token})|(.)'
+    
+    # Requires that we remove text, desc, class, id, and extra spaces in pre-processing
+    def tokenize(self, text):
+        matches = re.findall(self.pattern, text, re.DOTALL)
+        res = []
+        for group in matches:
+            if group[0]:
+                styles = re.findall(r'(style=)(")|(rgb|none|rotate|layer|matrix|default|visible|px|em|block|crosshair|translate)|([a-zA-Z0-9-]*:)|(.)', group[0], re.DOTALL)
+                for match in styles:
+                    for i in match:
+                        if i not in [None, '']:
+                            res.append(i)
+                continue
+            for match in group:
+                if match not in [None, '']:
+                    res.append(match)
 
-    return res
+        token_tensor = torch.tensor([self.get_id(token) for token in res])
+        return token_tensor
 
-def get_id(token):
-    global ctr
-    if token in token_to_id:
-        return token_to_id[token]
-    token_to_id[token] = ctr
-    ctr += 1
-    return token_to_id[token]
+    def get_id(self, token):
+    #    global ctr
+        if token in self.token_to_id:
+            return self.token_to_id[token]
+        self.token_to_id[token] = self.ctr
+        self.ctr += 1
+        return self.token_to_id[token]
+    
+    def save_tokenizer_meta_data(self, path=os.join('data/tokenizer_meta_data.json')):
+        meta_data = {'token_to_id': self.token_to_id,
+                     'ctr':         self.ctr}
+        data = json.dumps(meta_data, indent=4)
+ 
+        with open(path, "w") as outfile:
+            outfile.write(data)
+        outfile.close()
 
-class TextDataset(Dataset):
+class TextDataset(Dataset, ):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
 
         # List all files in the directory
         self.files = os.listdir(root_dir)
+        self.tokenizer = Tokenizer()
 
     def __len__(self):
         return len(self.files)
@@ -51,13 +70,15 @@ class TextDataset(Dataset):
             data_point = f.read()
 
         # Apply the custom tokenizer
-        data_point = custom_tokenizer(data_point)
+        data_point = self.tokenizer.tokenize(data_point)
+        self.tokenizer.save_tokenizer_meta_data()
 
         # Convert tokens to tensor
-        data_point = torch.tensor([get_id(token) for token in data_point])
+        #data_point = 
 
         if self.transform:
             data_point = self.transform(data_point)
+            
 
         return data_point
     
