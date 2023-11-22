ceab04cc59fb0ede5324c07cafd0d27fcb6da4f6
raghavm243512 <44511569+raghavm243512@users.noreply.github.com>
2023-11-22 08:23:39 -0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,21 @@
-from deep_translator import GoogleTranslator as Translator
 import re
 import os
 
 # Run once over all SVG
-
+# collapse whole file into 1 line with no excess spacing and regex removal of properties. 
 def translate_file(input_file, output_file):
-    translator = Translator()
-
     with open(input_file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
+    lines = ' '.join(lines)
+    lines = re.sub(r'<text.*?/text>', '', lines, flags=re.DOTALL)
+    lines = re.sub(r'<desc.*?/desc>', '', lines, flags=re.DOTALL)
+    lines = re.sub(r'(class|id)=".*?"', '', lines, flags=re.DOTALL)
+    lines = re.sub(r'\s+', ' ', lines)
     with open(output_file, 'w', encoding='utf-8') as f:
-        for line in lines:
-            chinese_phrases = re.findall(r'[\u4e00-\u9fff]+', line)
-            for phrase in chinese_phrases:
-                translation = translator.translate(phrase, source='zh-cn', target='en')
-                if 'layer' in line:
-                    translation = translation.upper().replace(' ', '_')
-                line = line.replace(phrase, translation)
-            f.write(line)
+        f.write(lines)
 
 path = 'data'
+out = 'out'
 for file in os.listdir(path):
-    translate_file(f'{path}{os.sep}{file}', f'{path}{os.sep}{file}')
+    translate_file(f'{path}{os.sep}{file}', f'{out}{os.sep}{file}')
