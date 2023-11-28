c8b31a1e50add46ba5476ca1b1a0b0e5bf62757f
Max-Edelson <meedelson@gmail.com>
2023-11-28 15:16:03 -0800

    Finished dataset

--- tokenizer.py
+++ tokenizer.py
@@ -5,6 +5,7 @@ import torch
 import json 
 from collections import defaultdict
 import numpy as np
+import time
 
 class Tokenizer(object):
     def __init__(self, tokenizer_meta_data=os.path.join('data', 'tokenizer_data','cubicasa_vocab_data.json')):
@@ -13,14 +14,17 @@ class Tokenizer(object):
         self.tokens_per_document = defaultdict(dict) # key = document -> returns dict of token counts per that document
         self.global_token_count = {}
         self.ctr = 1 # 0 used by pytorch for padding
-        '''if os.path.isfile(tokenizer_meta_data): # tokenizer_meta_data file already exists. Load it in
+        self.end_token = None
+        self.max_seq_len = 0
+        if os.path.isfile(tokenizer_meta_data): # tokenizer_meta_data file already exists. Load it in
             meta_data = json.load(open(tokenizer_meta_data,))
             self.token_to_id = meta_data['token_to_id']
             self.id_to_token = meta_data['id_to_token']
-            self.tokens_per_document = meta_data['tokens_per_document']
-            self.ctr = meta_data['ctr']'''
+            #self.tokens_per_document = meta_data['tokens_per_document']
+            self.ctr = meta_data['ctr']
+            self.end_token = meta_data['end_token']
+            self.max_seq_len = meta_data['max_seq_len']
         #self.start_token = re.escape('<?xml version="1.0"?>\n<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"')
-
         
         self.start_token1 = re.escape('<?xml version="1.0" encoding="UTF-8"?>\n<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"')
         self.start_token2 = re.escape('<?xml version="1.0"?>\n<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"')
@@ -71,6 +75,7 @@ class Tokenizer(object):
                 if match not in [None, '']:
                     res.append(match)'''
 
+        self.max_seq_len = max(self.max_seq_len, len(res))
         token_tensor = torch.tensor([self.get_id(token, file) for token in res])
         return token_tensor
 
@@ -95,7 +100,10 @@ class Tokenizer(object):
     
     def save_tokenizer_meta_data(self, path=os.path.join('data', 'tokenizer_data','cubicasa_vocab_data.json')):
         meta_data = {'token_to_id': self.token_to_id,
-                     'ctr':         self.ctr}
+                     'id_to_token': self.id_to_token,
+                     'ctr':         self.ctr,
+                     'end_token':   self.end_token,
+                     'max_seq_len': self.max_seq_len}
         data = json.dumps(meta_data, indent=4)
  
         with open(path, "w") as outfile:
@@ -129,58 +137,114 @@ class Tokenizer(object):
         self.save_tokenizer_meta_data()
 
 class TextDataset(Dataset, ):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
+    def __init__(self, end_token=181, transform=None, max_seq_len=99850):
+        #self.root_dir = root_dir
         self.transform = transform
+        self.seq_limit = 100000
+        self.end_token = end_token
 
         # List all files in the directory
-        self.files = os.listdir(root_dir)
+        self.files_json = f'svgs<{token_limit}.json' #os.listdir(root_dir)
+        self.files = None
+        with open(self.files_json, 'r') as openfile:
+            self.files = json.load(openfile)['small_files'] # gives a list of file names
         self.tokenizer = Tokenizer()
+        self.num_tokens = len(self.tokenizer.id_to_token) + 2 # one for end token, one for padding
 
     def __len__(self):
         return len(self.files)
-    
-    def tokenize_all_data(self):
-        for file in self.files:
-            if '.svg' not in file: continue
-            with open(os.path.join(self.root_dir, file), 'r') as f:
-                data_point = f.read()
-            data_point = self.tokenizer.tokenize(data_point, file)
-        self.tokenizer.write_vocab_to_file()
+
+    def pad_data(self, x, max_seq):
+        #print(f'max_seq: {max_seq}')
+        padded_data = torch.zeros(max_seq+1)
+        seq_len = x.shape[0]
+        padded_data[:seq_len] = x
+        padded_data[seq_len] = self.end_token
+        #print(f'padded data: {padded_data.shape}')
+        return padded_data
+
+    # padded_ex is of shape 1, max_seq_len
+    # return 1, max_seq_len, num tokens
+    def one_hot_encode(self, padded_ex, num_tokens):
+        padded_ex = padded_ex.squeeze()
+        eye = torch.eye(num_tokens)
+        padded_ex = padded_ex.int()
+    #    print(padded_ex)
+        eye = eye[padded_ex]
+        eye = eye.unsqueeze(0)
+        return eye
 
     def __getitem__(self, idx):
         # Open the file corresponding to idx
-        with open(os.path.join(self.root_dir, self.files[idx]), 'r') as f:
-            data_point = f.read()
+        with open(self.files[idx], 'r') as f:
+            text = f.read()
 
         file = self.files[idx]
 
-        # Apply the custom tokenizer
-        data_point = self.tokenizer.tokenize(data_point, file)
-        #self.tokenizer.save_tokenizer_meta_data()
-
-        # Convert tokens to tensor
-        #data_point = 
+        # Apply the custom tokenizer. returns a tensor
+        data_point = self.tokenizer.tokenize(text)
 
         if self.transform:
             data_point = self.transform(data_point)
-            
+
+        data_point = self.pad_data(data_point, self.tokenizer.max_seq_len)
+        data_point = self.one_hot_encode(padded_ex=data_point, num_tokens=self.num_tokens)    
 
         return data_point
-    
+
 if __name__ == '__main__':
-
+    tokenized_data = []
+    token_limit = 100000
+    '''
     tokenizer = Tokenizer()
+    #dataset = os.listdir(os.path.join('data','cubicasa5k','no_text_2'))
+    dataset = None
+    with open(f'svgs<{token_limit}.json', 'r') as openfile:
+        dataset = json.load(openfile)['small_files']
+    
+    small_files_list = []
 
-    with open('data/cubicasa5k/svgs/1_model.svg', 'r') as f:
-        text = ''.join(f.readlines())
+    t0 = time.time()
+    for file in dataset:
+        #file = os.path.join('data','cubicasa5k','no_text_2', file)
+        #print(f'file: {file}')
+        with open(file, 'r') as f:
+            text = ''.join(f.readlines())
+        ids = tokenizer.tokenize(text)
+        if ids.shape[0] < token_limit:
+            tokenized_data.append((ids, ids.shape[0]))
+            small_files_list.append(file)
+    t1 = time.time()
 
-    ids = tokenizer.tokenize(text)
+    end_token = tokenizer.ctr
+    tokenizer.end_token = end_token
+    tokenizer.ctr += 1
+    tokenizer.save_tokenizer_meta_data()
+    print(f'end_token: {end_token}')
+    small_file_obj = json.dumps({'small_files': small_files_list}, indent=4)
+ 
+    # Writing to sample.json
+    with open(f'svgs<{token_limit}.json', "w") as outfile:
+        outfile.write(small_file_obj)
+    outfile.close()
 
+    tokenized_data.sort(reverse=True, key=lambda x: x[1])
+    print(f'Max sequence length: {tokenized_data[0][1]}. Tokenization took {t1-t0} seconds. Contains {len(tokenized_data)} examples. end_token: {end_token}. Vocab Size: {tokenizer.ctr}.')
+    '''
+
+    #dataset = TextDataset()
+    #preprocessed_item = dataset[0]
+
+
+    '''
     un_tokenized = ''
     for id in ids:
         un_tokenized += tokenizer.get_token(id.item())
 
+    f = open('1_model_untokenized.svg', 'w')
+    f.write(un_tokenized)
+    f.close()
+    '''
     #print(un_tokenized)    
 #dataset = TextDataset(root_dir='data/cubicasa5k/svgs')
 #dataset.tokenize_all_data()
